6c74d2beaa152d6703f58f34564b9e968f386fd3
João Victor <joaovictorsalotialves12@gmail.com>
2025-04-18 11:16:53 -0300

    Configuração das documentação da API

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,33 @@ import fastifyJwt from '@fastify/jwt'
 import { usersRoutes } from './http/routers/userRouters'
 import { env } from './env'
 import { ZodError } from 'zod'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 
-export const app = fastify()
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Readify Plus',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
